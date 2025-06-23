783cd559c4e0c1990956e91dbec9d9549eab7552
nguyentri <trongtribk06@gmail.com>
2025-06-23 14:47:35 +0700

    remove backup + restructure samples\open_amp

--- samples/open_amp/cm7/sysbuild.cmake
+++ samples/open_amp/cm7/sysbuild.cmake
@@ -4,18 +4,18 @@
 
 # Add external project
 ExternalZephyrProject_Add(
-    APPLICATION openamp_remote
-    SOURCE_DIR ${APP_DIR}/remote
+    APPLICATION cm4
+    SOURCE_DIR ${APP_DIR}/../cm4
     BOARD ${SB_CONFIG_OPENAMP_REMOTE_BOARD}
   )
 
 # Add dependencies so that the remote sample will be built first
 # This is required because some primary cores need information from the
 # remote core's build, such as the output image's LMA
-add_dependencies(${DEFAULT_IMAGE} openamp_remote)
-sysbuild_add_dependencies(CONFIGURE ${DEFAULT_IMAGE} openamp_remote)
+add_dependencies(${DEFAULT_IMAGE} cm4)
+sysbuild_add_dependencies(CONFIGURE ${DEFAULT_IMAGE} cm4)
 
 if(SB_CONFIG_BOOTLOADER_MCUBOOT)
   # Make sure MCUboot is flashed first
-  sysbuild_add_dependencies(FLASH openamp_remote mcuboot)
+  sysbuild_add_dependencies(FLASH cm4 mcuboot)
 endif()
