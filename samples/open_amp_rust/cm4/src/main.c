3df549ba89084b6e7ccd19ee990b71a4240437b9
nguyentri <trongtribk06@gmail.com>
2025-06-23 22:18:44 +0700

    fix: change std console to uart1, change main.c in both cores to use rust code

--- samples/open_amp_rust/cm4/src/main.c
+++ samples/open_amp_rust/cm4/src/main.c
@@ -1,231 +1,135 @@
 /*
- * Copyright (c) 2018, NXP
- * Copyright (c) 2018, Nordic Semiconductor ASA
- * Copyright (c) 2018-2019, Linaro Limited
- *
+ * Copyright (c) 2024
  * SPDX-License-Identifier: Apache-2.0
  */
 
 #include <zephyr/kernel.h>
-#include <zephyr/drivers/ipm.h>
-#include <zephyr/sys/printk.h>
 #include <zephyr/device.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include <zephyr/logging/log.h>
+#include <zephyr/ipc/ipc_service.h>
 
-#include <openamp/open_amp.h>
+#ifdef CONFIG_OPENAMP_SYSBUILD
+#include <zephyr_image_info.h>
+#endif
 
-#include "common.h"
+// Register a logging module for this file (CM4 main)
+LOG_MODULE_REGISTER(cm4_main, LOG_LEVEL_INF);
 
-#define APP_TASK_STACK_SIZE (1024)
-K_THREAD_STACK_DEFINE(thread_stack, APP_TASK_STACK_SIZE);
-static struct k_thread thread_data;
+/*
+ * Rust FFI function declarations
+ * These are implemented in the Rust staticlib and linked into the Zephyr app.
+ * - rust_openamp_init_cm4: Initialize Rust-side OpenAMP state
+ * - rust_openamp_send_message_cm4: Log and count messages sent from Rust
+ * - rust_openamp_process_received_cm4: Process incoming messages in Rust
+ */
+extern void rust_openamp_init_cm4(void);
+extern int rust_openamp_send_message_cm4(const char* message);
+extern void rust_openamp_process_received_cm4(const char* data, size_t len);
 
-static const struct device *const ipm_handle =
-	DEVICE_DT_GET(DT_CHOSEN(zephyr_ipc));
+// IPC endpoint structure and state
+static struct ipc_ept ep;
+static bool endpoint_bound = false;
 
-static metal_phys_addr_t shm_physmap[] = { SHM_START_ADDR };
+// Callback: called when the endpoint is bound (ready for communication)
+static void ep_bound(void *priv)
+{
+    LOG_INF("CM4: Endpoint bound");
+    endpoint_bound = true;
+}
 
-static volatile unsigned int received_data;
+// Callback: called when a message is received from the remote core
+static void ep_recv(const void *data, size_t len, void *priv)
+{
+    LOG_INF("CM4: Received message: %.*s", (int)len, (char *)data);
 
-static struct virtio_vring_info rvrings[2] = {
-	[0] = {
-		.info.align = VRING_ALIGNMENT,
-	},
-	[1] = {
-		.info.align = VRING_ALIGNMENT,
-	},
+    // Forward the received message to Rust for further processing
+    rust_openamp_process_received_cm4((const char*)data, len);
+}
+
+// IPC endpoint configuration: name and callbacks
+static struct ipc_ept_cfg ep_cfg = {
+    .name = "ep0", // Endpoint name (must match remote side)
+    .cb = {
+        .bound = ep_bound,    // Called when endpoint is ready
+        .received = ep_recv,  // Called on message reception
+    },
 };
-static struct virtio_device vdev;
-static struct rpmsg_virtio_device rvdev;
-static struct metal_io_region shm_io_data;
-static struct metal_io_region *io = &shm_io_data;
-static struct virtqueue *vqueue[2];
-
-static unsigned char ipc_virtio_get_status(struct virtio_device *dev)
-{
-	return sys_read8(VDEV_STATUS_ADDR);
-}
-
-static uint32_t ipc_virtio_get_features(struct virtio_device *dev)
-{
-	return 1 << VIRTIO_RPMSG_F_NS;
-}
-
-static void ipc_virtio_notify(struct virtqueue *vq)
-{
-#if defined(CONFIG_SOC_MPS2_AN521) || \
-	defined(CONFIG_SOC_V2M_MUSCA_B1)
-	uint32_t current_core = sse_200_platform_get_cpu_id();
-
-	ipm_send(ipm_handle, 0, current_core ? 0 : 1, 0, 1);
-#else
-	uint32_t dummy_data = 0x00110011; /* Some data must be provided */
-
-	ipm_send(ipm_handle, 0, 0, &dummy_data, sizeof(dummy_data));
-#endif /* #if defined(CONFIG_SOC_MPS2_AN521) */
-}
-
-struct virtio_dispatch dispatch = {
-	.get_status = ipc_virtio_get_status,
-	.get_features = ipc_virtio_get_features,
-	.notify = ipc_virtio_notify,
-};
-
-static K_SEM_DEFINE(data_sem, 0, 1);
-static K_SEM_DEFINE(data_rx_sem, 0, 1);
-
-static void platform_ipm_callback(const struct device *dev, void *context,
-				  uint32_t id, volatile void *data)
-{
-	k_sem_give(&data_sem);
-}
-
-int endpoint_cb(struct rpmsg_endpoint *ept, void *data,
-		size_t len, uint32_t src, void *priv)
-{
-	received_data = *((unsigned int *) data);
-
-	k_sem_give(&data_rx_sem);
-
-	return RPMSG_SUCCESS;
-}
-
-static K_SEM_DEFINE(ept_sem, 0, 1);
-
-struct rpmsg_endpoint my_ept;
-struct rpmsg_endpoint *ep = &my_ept;
-
-static void rpmsg_service_unbind(struct rpmsg_endpoint *ept)
-{
-	(void)ept;
-	rpmsg_destroy_ept(ep);
-}
-
-static unsigned int receive_message(void)
-{
-	while (k_sem_take(&data_rx_sem, K_NO_WAIT) != 0) {
-		int status = k_sem_take(&data_sem, K_FOREVER);
-
-		if (status == 0) {
-			virtqueue_notification(vqueue[1]);
-		}
-	}
-	return received_data;
-}
-
-static int send_message(unsigned int message)
-{
-	return rpmsg_send(ep, &message, sizeof(message));
-}
-
-void app_task(void *arg1, void *arg2, void *arg3)
-{
-	ARG_UNUSED(arg1);
-	ARG_UNUSED(arg2);
-	ARG_UNUSED(arg3);
-
-	int status = 0;
-	unsigned int message = 0U;
-	struct rpmsg_device *rdev;
-	struct metal_init_params metal_params = METAL_INIT_DEFAULTS;
-
-	printk("\r\nOpenAMP[remote] demo started\r\n");
-
-	status = metal_init(&metal_params);
-	if (status != 0) {
-		printk("metal_init: failed - error code %d\n", status);
-		return;
-	}
-
-	/* declare shared memory region */
-	metal_io_init(io, (void *)SHM_START_ADDR, shm_physmap, SHM_SIZE, -1, 0, NULL);
-
-	/* setup IPM */
-	if (!device_is_ready(ipm_handle)) {
-		printk("IPM device is not ready\n");
-		return;
-	}
-
-	ipm_register_callback(ipm_handle, platform_ipm_callback, NULL);
-
-	status = ipm_set_enabled(ipm_handle, 1);
-	if (status != 0) {
-		printk("ipm_set_enabled failed\n");
-		return;
-	}
-
-	/* setup vdev */
-	vqueue[0] = virtqueue_allocate(VRING_SIZE);
-	if (vqueue[0] == NULL) {
-		printk("virtqueue_allocate failed to alloc vqueue[0]\n");
-		return;
-	}
-	vqueue[1] = virtqueue_allocate(VRING_SIZE);
-	if (vqueue[1] == NULL) {
-		printk("virtqueue_allocate failed to alloc vqueue[1]\n");
-		return;
-	}
-
-	vdev.role = RPMSG_REMOTE;
-	vdev.vrings_num = VRING_COUNT;
-	vdev.func = &dispatch;
-	rvrings[0].io = io;
-	rvrings[0].info.vaddr = (void *)VRING_TX_ADDRESS;
-	rvrings[0].info.num_descs = VRING_SIZE;
-	rvrings[0].info.align = VRING_ALIGNMENT;
-	rvrings[0].vq = vqueue[0];
-
-	rvrings[1].io = io;
-	rvrings[1].info.vaddr = (void *)VRING_RX_ADDRESS;
-	rvrings[1].info.num_descs = VRING_SIZE;
-	rvrings[1].info.align = VRING_ALIGNMENT;
-	rvrings[1].vq = vqueue[1];
-
-	vdev.vrings_info = &rvrings[0];
-
-	/* setup rvdev */
-	status = rpmsg_init_vdev(&rvdev, &vdev, NULL, io, NULL);
-	if (status != 0) {
-		printk("rpmsg_init_vdev failed %d\n", status);
-		return;
-	}
-
-	rdev = rpmsg_virtio_get_rpmsg_device(&rvdev);
-
-	status = rpmsg_create_ept(ep, rdev, "k", RPMSG_ADDR_ANY,
-			RPMSG_ADDR_ANY, endpoint_cb, rpmsg_service_unbind);
-	if (status != 0) {
-		printk("rpmsg_create_ept failed %d\n", status);
-		return;
-	}
-
-	while (message < 99) {
-		message = receive_message();
-		printk("Remote core received a message: %d\n", message);
-
-		message++;
-		status = send_message(message);
-		if (status < 0) {
-			printk("send_message(%d) failed with status %d\n",
-			       message, status);
-			goto _cleanup;
-		}
-	}
-
-_cleanup:
-	rpmsg_deinit_vdev(&rvdev);
-	metal_finish();
-
-	printk("OpenAMP demo ended.\n");
-}
 
 int main(void)
 {
-	printk("Starting application thread!\n");
-	k_thread_create(&thread_data, thread_stack, APP_TASK_STACK_SIZE,
-			app_task,
-			NULL, NULL, NULL, K_PRIO_COOP(7), 0, K_NO_WAIT);
-	return 0;
+    const struct device *ipc0_instance;
+    int ret;
+
+    LOG_INF("OpenAMP Rust CM4 Application started");
+
+#ifdef CONFIG_OPENAMP_SYSBUILD
+    LOG_INF("Running in sysbuild mode");
+    #ifdef CM7_IMAGE_SIZE
+    LOG_INF("CM7 image size: %d bytes", CM7_IMAGE_SIZE);
+    #endif
+#else
+    LOG_INF("Running in single-core mode");
+#endif
+
+    // Initialize Rust OpenAMP state (logging, counters, etc.)
+    rust_openamp_init_cm4();
+
+    // Get the IPC device instance (must match devicetree label)
+    ipc0_instance = DEVICE_DT_GET(DT_NODELABEL(ipc0));
+    if (!device_is_ready(ipc0_instance)) {
+        LOG_ERR("IPC instance is not ready");
+        return -ENODEV;
+    }
+
+    // Open the IPC service instance (idempotent)
+    ret = ipc_service_open_instance(ipc0_instance);
+    if (ret < 0 && ret != -EALREADY) {
+        LOG_ERR("Failed to open IPC instance: %d", ret);
+        return ret;
+    }
+
+    // Register the endpoint for communication
+    ret = ipc_service_register_endpoint(ipc0_instance, &ep, &ep_cfg);
+    if (ret < 0) {
+        LOG_ERR("Failed to register endpoint: %d", ret);
+        return ret;
+    }
+
+    // Wait for the endpoint to be bound by the remote core
+    LOG_INF("Waiting for endpoint to be bound...");
+    while (!endpoint_bound) {
+        k_sleep(K_MSEC(100));
+    }
+
+    LOG_INF("Endpoint bound, starting communication...");
+    k_sleep(K_SECONDS(1)); // Optional: allow remote to get ready
+
+    /*
+     * Main message loop:
+     * - Compose a message string
+     * - Log and count it in Rust (rust_openamp_send_message_cm4)
+     * - Send it over the IPC endpoint
+     * - Wait before sending the next message
+     */
+    for (int i = 0; i < 10; i++) {
+        char msg[64];
+        snprintf(msg, sizeof(msg), "Hello from CM4 (Rust) - Message %d", i);
+
+        // Log and count the message in Rust
+        ret = rust_openamp_send_message_cm4(msg);
+        if (ret < 0) {
+            LOG_ERR("Failed to send message via Rust: %d", ret);
+        } else {
+            // Send the message over the Zephyr IPC service
+            ret = ipc_service_send(&ep, msg, strlen(msg));
+            if (ret < 0) {
+                LOG_ERR("Failed to send IPC message: %d", ret);
+            }
+        }
+
+        k_sleep(K_SECONDS(2)); // Delay between messages
+    }
+
+    LOG_INF("CM4 communication test completed");
+    return 0;
 }
